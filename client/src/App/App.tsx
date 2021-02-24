aaa715706c03f99f0a54fc9ad211a36d464aa6aa
Dimby Rasolonirina <DRasolonirina@aveolys.loc>
2021-02-24 16:21:41 +0300

    first commit

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div>Bienvenu</div>;
+  return <div>
+    Bienvenue, je m'appelle Dimby
+  </div>;
 };
 
 export default App;
