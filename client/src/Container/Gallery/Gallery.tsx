db634fc0b0f4bfc8ff11572e5b9db88b6d8b87c2
Dimby Rasolonirina <dimbyrasolonirina@gmail.com>
2021-04-26 15:41:21 +0300

    page sary

--- client/src/Container/Gallery/Gallery.tsx
+++ client/src/Container/Gallery/Gallery.tsx
@@ -5,6 +5,7 @@ import fond from '../../Images/fond-1.jpg';
 import fg from '../../Images/pekin - Copie.jpg';
 import ambatondrazakaFitiavana from '../../Images/ambatondrazakaFitiavana.jpeg';
 import ZoomOutMapIcon from '@material-ui/icons/ZoomOutMap';
+import ChevronRight from '@material-ui/icons/ChevronRight';
 
 
 const Gallery : FC = () => {
@@ -20,10 +21,10 @@ const Gallery : FC = () => {
         </div>
         <div className={classes.boxTwo}>
             <Grid container spacing={0}>
-                <Grid xs>
+                <Grid xs style={{borderRight: "1px solid rgb(0 0 0 / 10%)"}}>
                     <div><h2 style={{marginLeft: "15px", color: "#F4C247"}}>SARY SY HORONANTSARY TAHIRY</h2></div>
                     <Grid container spacing={0}>
-                        <Grid xs={4}>
+                        <Grid xs={4} style={{marginBottom: "20px"}}>
                             <div className="responsive">
                                 <div className="gallery">
                                     <img src={ambatondrazakaFitiavana} alt=""/>
@@ -77,11 +78,133 @@ const Gallery : FC = () => {
                                 </div>
                             </div>
                         </Grid>
-
+                        <Grid xs={4}>
+                            <div className="responsive">
+                                <div className="gallery">
+                                    <img src={fg} alt=""/>
+                                    <Grid container className="desc">
+                                        <Grid item>
+                                            <div><h4 style={{margin: 0}}>TITRE DE LA PHOTO</h4></div>
+                                            <div>Ambatondrazaka Fitiavana, 15 Fevrier</div>
+                                        </Grid>
+                                        <Grid xs style={{textAlign: "right"}}>
+                                            <IconButton>
+                                                <ZoomOutMapIcon />
+                                            </IconButton>
+                                        </Grid>
+                                    </Grid>
+                                </div>
+                            </div>
+                        </Grid>
+                        <Grid xs={4}>
+                            <div className="responsive">
+                                <div className="gallery">
+                                    <img src={fond} alt=""/>
+                                    <Grid container className="desc">
+                                        <Grid item>
+                                            <div><h4 style={{margin: 0}}>TITRE DE LA PHOTO</h4></div>
+                                            <div>Ambatondrazaka Fitiavana, 15 Fevrier</div>
+                                        </Grid>
+                                        <Grid xs style={{textAlign: "right"}}>
+                                            <IconButton>
+                                                <ZoomOutMapIcon />
+                                            </IconButton>
+                                        </Grid>
+                                    </Grid>
+                                </div>
+                            </div>
+                        </Grid>
+                        <Grid xs={4}>
+                            <div className="responsive">
+                                <div className="gallery">
+                                    <img src={fg} alt=""/>
+                                    <Grid container className="desc">
+                                        <Grid item>
+                                            <div><h4 style={{margin: 0}}>TITRE DE LA PHOTO</h4></div>
+                                            <div>Ambatondrazaka Fitiavana, 15 Fevrier</div>
+                                        </Grid>
+                                        <Grid xs style={{textAlign: "right"}}>
+                                            <IconButton>
+                                                <ZoomOutMapIcon />
+                                            </IconButton>
+                                        </Grid>
+                                    </Grid>
+                                </div>
+                            </div>
+                        </Grid>
+                        <Grid xs={4}>
+                            <div className="responsive">
+                                <div className="gallery">
+                                    <img src={fond} alt=""/>
+                                    <Grid container className="desc">
+                                        <Grid item>
+                                            <div><h4 style={{margin: 0}}>TITRE DE LA PHOTO</h4></div>
+                                            <div>Ambatondrazaka Fitiavana, 15 Fevrier</div>
+                                        </Grid>
+                                        <Grid xs style={{textAlign: "right"}}>
+                                            <IconButton>
+                                                <ZoomOutMapIcon />
+                                            </IconButton>
+                                        </Grid>
+                                    </Grid>
+                                </div>
+                            </div>
+                        </Grid>
                     </Grid>
                 </Grid>
-                <Grid xs={3}>
-                    <div>xs</div>
+                <Grid xs={3} style={{paddingLeft: "20px"}}>
+                    <div><h2 style={{marginLeft: "15px", color: "#F4C247"}}>JEREO KOA</h2></div>
+                    <div className="news">
+                        <div className="item">
+                            <div className="content" style={{ borderLeft: "5px solid #006AB0",}}>
+                                <Grid container>
+                            <Grid item>
+                                <div><h4 style={{margin: 0}}>14 Juin 2021</h4></div>
+                                <div>Fambolenkazo teny Manakambahiny </div>
+                            </Grid>
+                            <Grid xs style={{textAlign: "right"}}>
+                                <IconButton style={{backgroundColor: "#F5F5F5"}}>
+                                    <ChevronRight />
+                                </IconButton>
+                            </Grid>
+                                </Grid>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="news">
+                        <div className="item">
+                            <div className="content" style={{ borderLeft: "5px solid #006AB0",}}>
+                                <Grid container>
+                            <Grid item>
+                                <div><h4 style={{margin: 0}}>TETIKANDRO</h4></div>
+                                <div>Taona 2021</div>
+                            </Grid>
+                            <Grid xs style={{textAlign: "right"}}>
+                                <IconButton style={{backgroundColor: "#F5F5F5"}}>
+                                    <ChevronRight />
+                                </IconButton>
+                            </Grid>
+                                </Grid>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="news">
+                        <div className="item">
+                            <div className="content" style={{ borderLeft: "5px solid #006AB0",}}>
+                                <Grid container>
+                            <Grid item>
+                                <div><h4 style={{margin: 0}}>FITSIPIPIFEHEZANA</h4></div>
+                                <div>Ireo tsara ho fantatra...</div>
+                            </Grid>
+                            <Grid xs style={{textAlign: "right"}}>
+                                <IconButton style={{backgroundColor: "#F5F5F5"}}>
+                                    <ChevronRight />
+                                </IconButton>
+                            </Grid>
+                                </Grid>
+                            </div>
+                        </div>
+                    </div>
                 </Grid>
             </Grid>
         </div>
--- client/src/Container/Gallery/style.tsx
+++ client/src/Container/Gallery/style.tsx
@@ -29,7 +29,25 @@ const useStyles = makeStyles((theme: Theme) =>
                         padding: "5px 15px",
                     }
                 }
-            }
+            },
+            "& .item": {
+                padding: "15px 20px",
+                "& :hover": {
+                    transform: "scale(1.005)",
+                    transition: "all 0.1s ease-in-out"
+                },
+                "& .content": {
+                    maxHeight: "78px",
+                    borderRadius: "4px",
+                    cursor: "pointer",
+                    padding: "15px 20px",
+                    boxShadow: "0 2px 5px 1px rgb(64 60 67 / 16%)",
+                    "& h2": {
+                        padding: "0",
+                        margin: "0"
+                    }
+                },
+            },
         },
 }));
 
