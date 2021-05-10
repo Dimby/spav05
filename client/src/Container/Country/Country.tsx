5ece95e5af492c0dbabe3aad2f389273cd0adf06
Dimby Rasolonirina <dimbyrasolonirina@gmail.com>
2021-05-10 09:17:09 +0300

    faritra

--- client/src/Container/Country/Country.tsx
+++ client/src/Container/Country/Country.tsx
@@ -1,37 +1,51 @@
 import React, { FC } from 'react'
-import styles from './style'
-import Slider from 'react-slick'
+import useStyles from './style'
+import { Grid, Link } from '@material-ui/core'
 
 const Country: FC = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  }
+  const classes = useStyles()
   return (
     <div style={{ marginTop: '50px' }}>
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
+      <div className={classes.boxOne}>
+        <Grid container spacing={0}>
+            <Grid xs={2}>
+              <div>
+                <h2 style={{ marginLeft: '15px', color: '#F4C247' }}>FARITRA</h2>
+                <div className="item" style={{ marginLeft: '20px' }}>
+                  <div className="menuItem">
+                    <Link>Ambatondrazaka</Link>
+                  </div>
+                  <div className="menuItem">
+                    <Link>Andilamena</Link>
+                  </div>
+                  <div className="menuItem">
+                    <Link>Andilanatoby</Link>
+                  </div>
+                  <div className="menuItem">
+                    <Link>Andreba</Link>
+                  </div>
+                  <div className="menuItem">
+                    <Link>Didy</Link>
+                  </div>
+                  <div className="menuItem">
+                    <Link>Imerimandroso</Link>
+                  </div>
+                </div>
+              </div>
+            </Grid>
+            <Grid style={{ padding: '20px' }} xs>
+              <h1>AMBATONDRAZAKA</h1>
+              <h1>ANDILAMENA</h1>
+              <h1>ANDILANATOBY</h1>
+              <h1>ANDREBA</h1>
+              <h1>DIDY</h1>
+              <h1>IMERIMANDROSO</h1>
+            </Grid>
+            <Grid xs={3}>
+              <div>xs</div>
+            </Grid>
+        </Grid>
+      </div>
     </div>
   )
 }
